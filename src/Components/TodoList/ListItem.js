282cfa67d7618a2cff00146bbc5656a912f89db2
Kristian Sakarisson <kristian@sakarisson.com>
2018-10-09 11:48:24 +0300

    Started implementing edit

--- src/Components/TodoList/ListItem.js
+++ src/Components/TodoList/ListItem.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { compose } from 'recompose';
+import { compose, withStateHandlers } from 'recompose';
 import { observer } from 'mobx-react';
 
-const ListItem = ({ item, destroy }) => {
+const ListItem = ({
+  item,
+  destroy,
+  startEditing,
+  editing,
+  setItemName,
+}) => {
   const { name, done, toggle } = item;
-  const completedClass = done ? 'completed' : null; 
+  const completedClass = done ? 'completed' : null;
+  const editingClass = editing ? 'editing' : null;
   return (
-    <li className={completedClass}>
-      <div className="view">
+    <li className={`${completedClass} ${editingClass}`}>
+      <div className="view" onDoubleClick={startEditing}>
         <input
           type="checkbox"
           className="toggle"
@@ -22,6 +29,11 @@ const ListItem = ({ item, destroy }) => {
           onClick={destroy}
         />
       </div>
+      <input
+        className="edit"
+        value={name}
+        onChange={setItemName}
+      />
     </li>
   );
 };
@@ -31,9 +43,26 @@ ListItem.propTypes = {
     name: PropTypes.string.isRequired,
     done: PropTypes.bool.isRequired,
   }).isRequired,
+  destroy: PropTypes.func.isRequired,
+  startEditing: PropTypes.func.isRequired,
+  setItemName: PropTypes.func.isRequired,
 };
 
+const editHandler = withStateHandlers(
+  () => ({ editing: false }),
+  {
+    startEditing: () => () => ({ editing: true }),
+    stopEditing: () => () => ({ editing: false }),
+    setItemName: (state, { item }) => (event) => {
+      const { target } = event;
+      const { value } = target;
+      item.setName(value);
+    },
+  }
+);
+
 const wrapper = compose(
+  editHandler,
   observer,
 );
 
