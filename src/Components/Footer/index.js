ab796619de4f0702190803cb4c779e37a8483883
Kristian Sakarisson <kristian@sakarisson.com>
2018-10-10 09:55:14 +0300

    Added filtering on frontend

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,23 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { observer } from 'mobx-react';
+
 import ItemCount from './ItemCount';
+
+const filters = {
+	all: '/',
+	active: '/active',
+	completed: '/completed',
+};
+
+const FilterLink = observer(({ store, filter }) => {
+	const { name, url } = filter;
+	const currentFilter = store.filter;
+	const active = filter.name === currentFilter;
+	const selectedClass = active ? 'selected' : '';
+	return (
+		<li>
+			<a
+				className={selectedClass}
+				href={`#${url}`}
+				onClick={() => store.filterBy(name)}
+			>
+				{name}
+			</a>
+		</li>
+	);
+});
+
+FilterLink.propTypes = {
+	store: PropTypes.object.isRequired,
+	filter: PropTypes.object.isRequired
+};
 
 const Footer = ({ store }) => (
   <footer className="footer">
     <ItemCount store={store} />
-				<ul className="filters">
-					<li>
-						<a className="selected" href="#/">All</a>
-					</li>
-					<li>
-						<a href="#/active">Active</a>
-					</li>
-					<li>
-						<a href="#/completed">Completed</a>
-					</li>
-				</ul>
-      <button className="clear-completed">Clear completed</button>
+			<ul className="filters">
+				{Object.keys(filters).map(key => (
+					<FilterLink
+						filter={{ name: key, url: filters[key] }}
+						store={store}
+						key={key}
+					/>
+				))}
+			</ul>
+		<button className="clear-completed">Clear completed</button>
   </footer>
 );
 
